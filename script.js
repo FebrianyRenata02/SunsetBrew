9ec850e573251c4d2ec736c91ab2587258eb925e
Febriany Renata <96133771+FebrianyRenata02@users.noreply.github.com>
2026-03-07 19:56:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -377,7 +377,7 @@ menuData.forEach(item => {
     img.src = item.image;
 
     img.className =
-        "w-full h-[320px] object-cover";
+        "w-full h-[220px] object-cover";
 
     const title = document.createElement("h3");
 
